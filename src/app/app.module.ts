57f49a000da105f2ffda05afecc067b07bbc1995
Carol254 <wanguic49@gmail.com>
2023-04-18 23:20:26 +0300

    few changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LogInComponent } from './main/log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule
